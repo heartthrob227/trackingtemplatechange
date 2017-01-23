74aa814bab0a14b43cd65c446ad72c45b2e7a4e1
heartthrob227 <dennismyu51@gmail.com>
2017-01-22 23:34:07 -0500

    big change

--- app/views.py
+++ app/views.py
@@ -10,10 +10,8 @@ e.focus_set()
 
 @app.route('/')
 @app.route('/index')
-@app.route('/input')
+
 
 def index():
     return "Hello, World!"
 	
-def input():
-	print e.get()
--- flask/Scripts/activate.bat
+++ flask/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\Users\kevin\flask"
+set "VIRTUAL_ENV=C:\Users\Dennis\desktop\kstracking\flask"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
